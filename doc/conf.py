05176f98848e34b2517465e75b37d43279c31e52
Martijn Vermaat <martijn@vermaat.name>
2016-02-14 09:19:59 +0100

    Show GitHub banner in docs

--- doc/conf.py
+++ doc/conf.py
@@ -130,7 +130,11 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_banner': True,
+    'github_user': 'martijnvermaat',
+    'github_repo': 'calmap'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
